e323d64d5fc2da6d2cedc888520b85b55fd37c39
Tai Zhang <q6112345@gmail.com>
2018-07-30 03:16:06 +0800

    Update example

--- Example/sync-example-to-root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Author: Bell
-cp -a ./node_modules/react-native-advanced-webview/*.js ../
-cp -a ./node_modules/react-native-advanced-webview/android/src ../android/
-cp -a ./node_modules/react-native-advanced-webview/ios/* ../ios/
-
--- Example/sync-root-to-example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Author: Bell
-cp -a ../*.js ./node_modules/react-native-advanced-webview/
-cp -a ../android/src ./node_modules/react-native-advanced-webview/android/
-cp -a ../ios/* ./node_modules/react-native-advanced-webview/ios/
-
